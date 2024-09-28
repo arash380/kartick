57e402eae0131772a690a5307f734513c5753356
Amir Khamesy <iknubs@gmail.com>
2024-09-28 13:00:02 -0700

    fixing column issue:

--- frontend/src/components/pages/Create/Create.js
+++ frontend/src/components/pages/Create/Create.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import classes from "./Create.module.css";
-import { addDoc } from "firebase/firestore";
-import { lobbiesCollection } from "../../../firebase/firebase";
 import logo from "../../../assets/images/logo.png";
 
 const Create = () => {
@@ -9,33 +7,19 @@ const Create = () => {
 
   const isCreateDisabled = playerName.trim() === "";
 
-  const onCreate = async (event) => {
-    event.preventDefault();
-
-    await addDoc(lobbiesCollection, {
-      startedAt: new Date(),
-      active: true,
-      round: 0, // game has not started yet?
-    });
-  };
-
   return (
     <div className={classes.centered}>
-      <form onSubmit={onCreate}>
-        <img src={logo} alt="Logo" className={classes.logo} />
+      <img src={logo} alt="Logo" className={classes.logo} />
 
-        <label htmlFor="playerName">Player Name</label>
-        <input
-          type="text"
-          id="playerName"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-        />
+      <label htmlFor="playerName">Player Name</label>
+      <input
+        type="text"
+        id="playerName"
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+      />
 
-        <button disabled={isCreateDisabled} type="submit">
-          Join Lobby
-        </button>
-      </form>
+      <button disabled={isCreateDisabled}>Join Lobby</button>
     </div>
   );
 };

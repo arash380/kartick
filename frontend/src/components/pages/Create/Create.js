dc68201916217c2fcf682a0113fba544ae84c186
asa510 <arash_saadati@sfu.ca>
2024-09-28 12:55:46 -0700

    added lobby create logic

--- frontend/src/components/pages/Create/Create.js
+++ frontend/src/components/pages/Create/Create.js
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import classes from "./Create.module.css";
+import { addDoc } from "firebase/firestore";
+import { lobbiesCollection } from "../../../firebase/firebase";
 
 const Create = () => {
   const [playerName, setPlayerName] = useState("");
 
   const isCreateDisabled = playerName.trim() === "";
 
+  const onCreate = async (event) => {
+    event.preventDefault();
+
+    await addDoc(lobbiesCollection, {
+      startedAt: new Date(),
+      active: true,
+      round: 0, // game has not started yet?
+    });
+  };
+
   return (
     <div className={classes.centered}>
-      <label htmlFor="playerName">Player Name</label>
-      <input
-        type="text"
-        id="playerName"
-        value={playerName}
-        onChange={(e) => setPlayerName(e.target.value)}
-      />
+      <form onSubmit={onCreate}>
+        <label htmlFor="playerName">Player Name</label>
+        <input
+          type="text"
+          id="playerName"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
 
-      <button disabled={isCreateDisabled}>Join Lobby</button>
+        <button disabled={isCreateDisabled} type="submit">
+          Join Lobby
+        </button>
+      </form>
     </div>
   );
 };

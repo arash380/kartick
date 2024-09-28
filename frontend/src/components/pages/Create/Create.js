24f38a3b578b3b42ad2c0878bc0a934dbb7c96ad
Tmhduc <duc_ta@sfu.ca>
2024-09-28 13:37:47 -0700

    add back paga icon

--- frontend/src/components/pages/Create/Create.js
+++ frontend/src/components/pages/Create/Create.js
@@ -3,8 +3,11 @@ import classes from "./Create.module.css";
 import logo from "../../../assets/images/logo.png";
 import { addDoc } from "firebase/firestore";
 import { lobbiesCollection } from "../../../firebase/firebase";
+import { useNavigate } from "react-router-dom";
+import rc from "../../routing/routeConfigs";
 
 const Create = () => {
+  const navigate = useNavigate();
   const [playerName, setPlayerName] = useState("");
 
   const isCreateDisabled = playerName.trim() === "";
@@ -24,22 +27,25 @@ const Create = () => {
   };
 
   return (
-    <div className={classes.centered}>
-      <img src={logo} alt="Logo" className={classes.logo} />
+    <div className={classes.roots}>
+      <a className={classes.header} onClick={() => navigate(rc.default)}>Kartick.io</a>
+      <div className={classes.centered}>
+        <img src={logo} alt="Logo" className={classes.logo} />
 
-      <form onSubmit={onCreate}>
-        <label htmlFor="playerName">Player Name</label>
-        <input
-          type="text"
-          id="playerName"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-        />
+        <form onSubmit={onCreate}>
+          <label htmlFor="playerName">Player Name</label>
+          <input
+            type="text"
+            id="playerName"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
 
-        <button disabled={isCreateDisabled} type="submit">
-          Join Lobby
-        </button>
-      </form>
+          <button disabled={isCreateDisabled} type="submit">
+            Join Lobby
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/components/pages/Join/Join.js
+++ frontend/src/components/pages/Join/Join.js
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 import classes from "./Join.module.css";
+import { useNavigate } from "react-router-dom";
+import rc from "../../routing/routeConfigs";
+
 import logo from "../../../assets/images/logo.png";
 
 const Join = () => {
+  const navigate = useNavigate();
   const [roomLobby, setRoomLobby] = useState("");
   const [playerName, setPlayerName] = useState("");
 
   const isJoinDisabled = roomLobby.trim() === "" || playerName.trim() === "";
 
   return (
-    <div className={classes.centered}>
-      <img src={logo} alt="Logo" className={classes.logo} />
+    <div className={classes.root}>
+      <a className={classes.header} onClick={() => navigate(rc.default  )}>Kartick.io</a>
+      <div className={classes.centered}>
+        <img src={logo} alt="Logo" className={classes.logo} />
 
-      <label htmlFor="roomLobby">Room Lobby</label>
-      <input
-        type="text"
-        id="roomLobby"
-        value={roomLobby}
-        onChange={(e) => setRoomLobby(e.target.value)}
-      />
+        <label htmlFor="roomLobby">Room Lobby</label>
+        <input
+          type="text"
+          id="roomLobby"
+          value={roomLobby}
+          onChange={(e) => setRoomLobby(e.target.value)}
+        />
 
-      <label htmlFor="playerName">Player Name</label>
-      <input
-        type="text"
-        id="playerName"
-        value={playerName}
-        onChange={(e) => setPlayerName(e.target.value)}
-      />
+        <label htmlFor="playerName">Player Name</label>
+        <input
+          type="text"
+          id="playerName"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
 
-      <button disabled={isJoinDisabled}>Join Lobby</button>
+        <button disabled={isJoinDisabled}>Join Lobby</button>
+
+      </div>
     </div>
   );
 };

c17d248e67183d486f3d2632a618d7301f5d3fa1
Amir Khamesy <iknubs@gmail.com>
2024-09-28 11:54:03 -0700

    adding api controller

--- frontend/src/services/api/cars.js
+++ frontend/src/services/api/cars.js
@@ -1,3 +1,22 @@
 import axios from "axios";
 
-// const URL_PREFIX = process.env.REACT_APP_API_URL;
+const URL_PREFIX = process.env.REACT_APP_API_URL;
+
+export const getRandomCars = async (numCars, minYear, includeImages) => {
+  const response = await axios.get(
+    `${URL_PREFIX}/random-cars?numCars=${numCars}&minYear=${minYear}&includeImages=${includeImages}`
+  );
+  return response.data;
+};
+
+//example usage in useEffect
+
+// const getRandomCarExample = async () => {
+//     const data = await getRandomCars(1, 2022, false);
+//     const carData = data[0];
+//     setRandomCar({
+//       name: carData?.name || "Unknown Car",
+//       img: carData?.image,
+//     });
+//   };
+//   getRandomCarExample();

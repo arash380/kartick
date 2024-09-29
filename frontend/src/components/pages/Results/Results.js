be962a6f3769c89ce8522b3d9ece0ba7e0d1973b
asa510 <arash_saadati@sfu.ca>
2024-09-28 17:13:11 -0700

    updated the results

--- frontend/src/components/pages/Results/Results.js
+++ frontend/src/components/pages/Results/Results.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Results.module.css";
+import { doc, onSnapshot } from "firebase/firestore";
+import { lobbiesCollection } from "../../../firebase/firebase";
+import rc from "../../routing/routeConfigs";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Results = () => {
-  return <div className={classes.root}>Results</div>;
+  const { lobbyId } = useParams();
+  const [lobby, setLobby] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let unsubscribe = () => {};
+    if (lobbyId) {
+      unsubscribe = onSnapshot(doc(lobbiesCollection, lobbyId), (doc) => {
+        setLobby(doc.data());
+        console.log(doc.data());
+
+        if (!doc.exists()) {
+          navigate(rc.default);
+        }
+      });
+    }
+
+    return () => {
+      setLobby(null);
+      unsubscribe();
+    };
+  }, [lobbyId, navigate]);
+
+  return <div className={classes.root}>results</div>;
 };
 
 export default Results;
